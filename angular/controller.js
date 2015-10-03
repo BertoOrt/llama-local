f11852017c6e60622c979016b47f724c4a11a19b
Roberto Ortega <berto.ort@gmail.com>
2015-10-03 10:38:41 -0600

    user page set

--- angular/controller.js
+++ angular/controller.js
@@ -1,7 +1,7 @@
 app.controller('home', ['$scope', function ($scope) {
   $scope.message = 'world';
   $scope.modal = function () {
-    $('.ui.basic.modal')
+    $('.ui.basic.modal.home')
     .modal('show');
   }
 }])
@@ -19,8 +19,11 @@ app.controller('home', ['$scope', function ($scope) {
 
 app.controller('signup', ['$scope', function ($scope) {
   $scope.modal = function () {
-    $('.ui.basic.modal')
+    $('.ui.basic.modal.signup')
     .modal('show');
+  };
+  $scope.dropdown = function () {
+    $('.ui.dropdown').dropdown();
   }
 }])
 
@@ -31,6 +34,7 @@ app.controller('user', ['$scope', function ($scope) {
   $scope.tab = function () {
     $('.tabular.menu .item').tab();
     $('.ui.accordion').accordion();
+    $('.ui.dropdown').dropdown();
   };
   $scope.pacTab = function () {
     $('.menu .item').tab();
